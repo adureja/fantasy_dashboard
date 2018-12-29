c91dbf051d70ab6c75941a14d44f30dcd0c1bd83
Avik Dureja <adurejabackup@gmail.com>
2018-12-29 13:10:05 -0500

    adding logging to flask

--- app.py
+++ app.py
@@ -15,6 +15,8 @@ import time
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+app.logger.addHandler(logging.StreamHandler(sys.stdout))
+app.logger.setLevel(logging.ERROR)
 db = SQLAlchemy(app)
 
 q = Queue(connection=conn)
